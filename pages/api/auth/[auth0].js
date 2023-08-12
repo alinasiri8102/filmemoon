e831f51fa35c52c649986a08456c12442584329f
Ali Nasiri <alinasiri8102@gmail.com>
2023-08-12 20:30:52 +0330

    remove auth0 baseUrl

--- pages/api/auth/[auth0].js
+++ pages/api/auth/[auth0].js
@@ -1,3 +1,13 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { initAuth0 } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+// export default handleAuth();
+
+export const initializeAuth0 = (req) => {
+  return initAuth0({
+    baseURL: req.headers.origin || `${process.env.NODE_ENV === "development" ? "http" : "https"}://${req.headers.host}`,
+  });
+};
+export default function auth(req, res) {
+  const auth0 = initializeAuth0(req);
+  return auth0.handleAuth()(req, res);
+}
