9570938364fee51aed402f37f96f1c05b2f4a80e
Ali Nasiri <alinasiri8102@gmail.com>
2023-08-13 19:14:34 +0330

    fix some style

--- pages/Header.jsx
+++ pages/Header.jsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { IconMovie } from "@tabler/icons-react";
 function Header() {
-  const { user, error, isLoading } = useUser();
+  const { user, isLoading } = useUser();
 
   return (
     <header className="fix-width flex-h">
       <Link href="/" className="logo">
         <h1 className="flex-h">
-          <IconMovie />
+          <IconMovie stroke={1} className="pr-color" />
           <span>Filmem</span>
           <span className="pr-color">oo</span>
           <span>n</span>
@@ -21,15 +21,15 @@ function Header() {
         <div className="profile flex-h">
           {user ? (
             <>
-              <a href="/api/auth/logout">
-                <IconLogout2 />
+              <a href="/api/auth/logout" className="logout">
+                <IconLogout2 size={22} />
               </a>
               <p className="name">{user.name || user.email}</p>
               <img src={user.picture} alt="" />
             </>
           ) : (
             <a className="btn btn-white" href="/api/auth/login">
-              <IconLogin />
+              <IconLogin size={22} />
               Login
             </a>
           )}
