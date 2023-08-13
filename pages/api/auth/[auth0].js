063b1b207c5d10d620e6ca83f5c0471d632c20b1
Ali Nasiri <alinasiri8102@gmail.com>
2023-08-13 15:00:15 +0330

    undo auth0 baseUrl

--- pages/api/auth/[auth0].js
+++ pages/api/auth/[auth0].js
@@ -1,13 +1,3 @@
-import { initAuth0 } from "@auth0/nextjs-auth0";
+import { handleAuth } from "@auth0/nextjs-auth0";
 
-// export default handleAuth();
-
-export const initializeAuth0 = (req) => {
-  return initAuth0({
-    baseURL: req.headers.origin || `${process.env.NODE_ENV === "development" ? "http" : "https"}://${req.headers.host}`,
-  });
-};
-export default function auth(req, res) {
-  const auth0 = initializeAuth0(req);
-  return auth0.handleAuth()(req, res);
-}
+export default handleAuth();
