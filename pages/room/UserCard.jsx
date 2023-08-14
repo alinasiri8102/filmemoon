2706d8d78b98a25871de61a54b1c55c1156d9319
Ali Nasiri <alinasiri8102@gmail.com>
2023-08-15 01:56:31 +0330

    fix userCard

--- pages/room/UserCard.jsx
+++ pages/room/UserCard.jsx
@@ -4,7 +4,7 @@ const UserCard = (props) => {
     <div className="member-card flex-h">
       <img src={user?.picture} />
       <div className="user-detail">
-        <p className="name">{user.given_name ? user.given_name + " " + user.family_name : user.nickname}</p>
+        <p className="name">{user?.given_name ? user.given_name + " " + user.family_name : user?.nickname}</p>
         <small className="email">{user.email}</small>
       </div>
     </div>
